c2c0021c5860b81624cae4846ee57a236c297ab4
faridepc78 <faridnewepc78@gmail.com>
2021-08-24 22:30:14 +0430

    new_refactor_commit

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -7,12 +7,9 @@ use App\Http\Requests\Auth\ForgotRequest;
 use App\Notifications\SendPasswordCode;
 use App\Repositories\UserRepository;
 use Exception;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
 {
-    use SendsPasswordResetEmails;
-
     private $userRepository;
 
     public function __construct(UserRepository $userRepository)
